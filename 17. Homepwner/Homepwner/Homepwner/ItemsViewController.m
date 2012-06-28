f65c56443915fd0e409267b20467b04a65b6c02d
Adam Preble <preble@gmail.com>
2012-06-28 16:48:18 -0400

    Added missing code to check for UIUserInterfaceIdiomPad before presenting popover.

--- 17. Homepwner/Homepwner/Homepwner/ItemsViewController.m
+++ 17. Homepwner/Homepwner/Homepwner/ItemsViewController.m
@@ -161,32 +161,34 @@
 {
     NSLog(@"Going to show the image for %@", ip);
 
-    // Get the item for the index path
-    BNRItem *i = [[[BNRItemStore sharedStore] allItems] objectAtIndex:[ip row]];
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad) {
+        // Get the item for the index path
+        BNRItem *i = [[[BNRItemStore sharedStore] allItems] objectAtIndex:[ip row]];
 
-    NSString *imageKey = [i imageKey];
+        NSString *imageKey = [i imageKey];
 
-    // If there is no image, we don't need to display anything
-    UIImage *img = [[BNRImageStore sharedStore] imageForKey:imageKey];
-    if(!img)
-        return;
-    
-    // Make a rectangle that the frame of the button relative to 
-    // our table view
-    CGRect rect = [[self view] convertRect:[sender bounds] fromView:sender];
-    
-    // Create a new ImageViewController and set its image
-    ImageViewController *ivc = [[ImageViewController alloc] init];
-    [ivc setImage:img];
-    
-    // Present a 600x600 popover 
-    imagePopover = [[UIPopoverController alloc] initWithContentViewController:ivc];
-    [imagePopover setDelegate:self];
-    [imagePopover setPopoverContentSize:CGSizeMake(600, 600)];
-    [imagePopover presentPopoverFromRect:rect 
-                                  inView:[self view] 
-                permittedArrowDirections:UIPopoverArrowDirectionAny 
-                                animated:YES];
+        // If there is no image, we don't need to display anything
+        UIImage *img = [[BNRImageStore sharedStore] imageForKey:imageKey];
+        if(!img)
+            return;
+        
+        // Make a rectangle that the frame of the button relative to 
+        // our table view
+        CGRect rect = [[self view] convertRect:[sender bounds] fromView:sender];
+        
+        // Create a new ImageViewController and set its image
+        ImageViewController *ivc = [[ImageViewController alloc] init];
+        [ivc setImage:img];
+        
+        // Present a 600x600 popover 
+        imagePopover = [[UIPopoverController alloc] initWithContentViewController:ivc];
+        [imagePopover setDelegate:self];
+        [imagePopover setPopoverContentSize:CGSizeMake(600, 600)];
+        [imagePopover presentPopoverFromRect:rect 
+                                      inView:[self view] 
+                    permittedArrowDirections:UIPopoverArrowDirectionAny 
+                                    animated:YES];
+    }
 }
 - (void)popoverControllerDidDismissPopover:(UIPopoverController *)popoverController
 {
